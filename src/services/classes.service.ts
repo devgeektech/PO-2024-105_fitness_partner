667c90f680a67418ea4e552d7d0a6717bba7719f
za-Salman <salman@geekinformatic.com>
2024-12-23 19:05:48 +0530

    edit class integration

--- src/services/classes.service.ts
+++ src/services/classes.service.ts
@@ -9,6 +9,9 @@ export const addClass = async (payload: any) => {
     return http.post(`/partner/addClass`, payload);
 }
 
+export const editClass = async (id: any, payload: any) => {
+    return http.put(`/partner/editClass/${id}`, payload);
+}
 
 export const getClassDetails = async (id:any) => {
     return http.get(`/partner/class/${id}`);
